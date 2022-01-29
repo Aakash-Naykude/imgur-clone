2357954fa5ee324076541a72c15bc317db5676b7
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-01-29 15:52:42 +0530

    added

---
+++ index.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".infinite-scroll");
+var pagenumber = 1;
+
+const loadImage = async () => {
+  let res = await fetch(
+    `https://api.unsplash.com/photos/?client_id=Xq62kux9RBMODPUloapAwC1eHiCNZRVh6zaeZk0K8ZM&per_page=15&page=${pagenumber}`
+  );
+  let data = await res.json();
+  showImage(data);
+};
+loadImage();
+function showImage(items) {
+  items.forEach((item) => {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = item.urls.small;
+    let title = document.createElement("h1");
+    div.append(img, title);
+    container.insertAdjacentElement("beforeend", div);
+  });
+}
+const showData = () => {
+  setTimeout(() => {
+    pagenumber++;
+    console.log(pagenumber);
+    loadImage();
+  }, 200);
+};
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    showData();
+  }
+});
